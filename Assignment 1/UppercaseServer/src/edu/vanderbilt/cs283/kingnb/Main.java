e30cdce2222466e9d27ca75c0df84237538c5c9e
Nick King <nicholas.b.king@vanderbilt.edu>
2014-02-12 02:53:26 -0600

    Fixed UppercaseServer directory structure

--- Assignment 1/UppercaseServer/src/edu/vanderbilt/cs283/kingnb/Main.java
+++ Assignment 1/UppercaseServer/src/edu/vanderbilt/cs283/kingnb/Main.java
@@ -1,14 +1,22 @@
 package edu.vanderbilt.cs283.kingnb;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner input = new Scanner(System.in);
-        System.out.println("Multithreaded server? (y/n) ");
-        if (input.next().toLowerCase().startsWith("y"));
-        
+        Server server;
+        System.out.print("Multithreaded server? (y/n) ");
+        if (input.next().toLowerCase().startsWith("y")) {
+        	System.out.println("Starting multithreaded server on port 8000");
+        	server = new MTServer(8000);
+        } else {
+        	System.out.println("Starting single threaded server on port 8000");
+        	server = new SimpleServer(8000);
+        }
+        server.serve();
     }
 
 }
--- Assignment 1/UppercaseServer/src/edu/vanderbilt/cs283/kingnb/SimpleServer.java
+++ Assignment 1/UppercaseServer/src/edu/vanderbilt/cs283/kingnb/SimpleServer.java
@@ -1,32 +1,30 @@
 package edu.vanderbilt.cs283.kingnb;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class SimpleServer {
-    
-    private ServerSocket mSocket;
+public class SimpleServer extends UppercaseServer {
 
-    SimpleServer(int port) throws IOException {
-        mSocket = new ServerSocket(port);
-        while (true) {
-            Socket socket = mSocket.accept();
-            BufferedReader rdr = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            String input;
-            StringBuilder output = new StringBuilder();
-            while ((input = rdr.readLine()) != null) {
-                System.out.println("Received input: " + input);
-                if (output.length() > 0) output.append("\n");
-                output.append(input.toUpperCase());
-            }
-            String outStr = output.toString();
-            System.out.println("Sending output: " + outStr);
-        }
-    }
-    
-    
+	private ServerSocket mSocket;
+
+	SimpleServer(int port) throws IOException {
+		mSocket = new ServerSocket(port);
+	}
+
+	@Override
+	public void serve() {
+		while (true) {
+			Socket socket = null;
+			try {
+				socket = mSocket.accept();
+				// socket will be closed by echoUppercase
+				echoUppercase(socket);
+			} catch (IOException e) {
+				System.err.println("Error accepting socket connection");
+				e.printStackTrace();
+			}
+		}
+	}
 
 }
