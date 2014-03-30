7a5caa4793d16b2e8b16b311b741cf6a1bafbc01
Nicholas King <nicholas.b.king@vanderbilt.edu>
2014-03-30 15:23:02 -0500

    Added TicTacToeClient

---
+++ TicTacToeClient/gen/edu/vanderbilt/cs283/kingnb/tictactoe/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package edu.vanderbilt.cs283.kingnb.tictactoe;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
