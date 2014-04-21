4ea67ee865e1fcdbf65303fcb83efb0e1f75eb7a
Nick King <nicholas.b.king@vanderbilt.edu>
2014-04-20 21:58:14 -0500

    Added Windows notification app

---
+++ PubNotifier/PubNotifier/OrderEntryForm.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace PubNotifier
+{
+    partial class OrderEntryForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.orderTextBox = new System.Windows.Forms.TextBox();
+            this.postOrderButton = new System.Windows.Forms.Button();
+            this.orderListBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // orderTextBox
+            // 
+            this.orderTextBox.Location = new System.Drawing.Point(12, 215);
+            this.orderTextBox.Name = "orderTextBox";
+            this.orderTextBox.Size = new System.Drawing.Size(244, 20);
+            this.orderTextBox.TabIndex = 0;
+            // 
+            // postOrderButton
+            // 
+            this.postOrderButton.Location = new System.Drawing.Point(262, 215);
+            this.postOrderButton.Name = "postOrderButton";
+            this.postOrderButton.Size = new System.Drawing.Size(75, 23);
+            this.postOrderButton.TabIndex = 1;
+            this.postOrderButton.Text = "Post Order";
+            this.postOrderButton.UseVisualStyleBackColor = true;
+            this.postOrderButton.Click += new System.EventHandler(this.postOrderButton_Click);
+            // 
+            // orderListBox
+            // 
+            this.orderListBox.FormattingEnabled = true;
+            this.orderListBox.Location = new System.Drawing.Point(12, 12);
+            this.orderListBox.Name = "orderListBox";
+            this.orderListBox.Size = new System.Drawing.Size(320, 199);
+            this.orderListBox.TabIndex = 2;
+            // 
+            // OrderEntryForm
+            // 
+            this.AcceptButton = this.postOrderButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(344, 253);
+            this.Controls.Add(this.orderListBox);
+            this.Controls.Add(this.postOrderButton);
+            this.Controls.Add(this.orderTextBox);
+            this.Name = "OrderEntryForm";
+            this.Text = "Pub Order Entry";
+            this.Load += new System.EventHandler(this.OrderEntryForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox orderTextBox;
+        private System.Windows.Forms.Button postOrderButton;
+        private System.Windows.Forms.ListBox orderListBox;
+    }
+}
+
---
+++ PubNotifier/PubNotifier/OrderEntryForm.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PubNotifier
+{
+    public partial class OrderEntryForm : Form
+    {
+        private List<int> _orderNums = new List<int>();
+        private const int MAX_NUM_ORDERS = 75;
+        private const int MAX_ORDER_VALUE = 10000;
+
+        public OrderEntryForm()
+        {
+            InitializeComponent();
+        }
+
+        private void postOrderButton_Click(object sender, EventArgs e)
+        {
+            SubmitText();
+        }
+
+        private void OrderEntryForm_Load(object sender, EventArgs e)
+        {
+            orderListBox.DataSource = _orderNums;
+        }
+
+        private void SubmitText()
+        {
+            int num;
+            try
+            {
+                num = Convert.ToInt32(orderTextBox.Text);
+                if (num > MAX_ORDER_VALUE)
+                {
+                    MessageBox.Show("Enter a number smaller than " + MAX_ORDER_VALUE, "Invalid input", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+                else if (num < 0)
+                {
+                    MessageBox.Show("Enter a nonnegative number", "Invalid input", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+                else
+                {
+                    _orderNums.Insert(0, num);
+                    if (_orderNums.Count > MAX_NUM_ORDERS)
+                    {
+                        _orderNums.RemoveAt(MAX_NUM_ORDERS);
+                    }
+                    orderTextBox.Clear();
+                    // Force the listbox to refresh its content
+                    orderListBox.DataSource = null;
+                    orderListBox.DataSource = _orderNums;
+                    Worker worker = new Worker(num);
+                    Thread workerThread = new Thread(worker.DoWork);
+
+                    // Start the worker thread.
+                    workerThread.Start(); 
+                }
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Please enter a number", "Invalid input", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+    }
+
+    public class Worker
+    {
+        private const string SERVER_IP = "54.186.61.49";
+        private const int SERVER_PORT = 20050;
+        private int _n;
+
+        public Worker(int n)
+        {
+            _n = n;
+        }
+
+        public void DoWork()
+        {
+            try
+            {
+                TcpClient client = new TcpClient(SERVER_IP, SERVER_PORT);
+                StreamWriter writer = new StreamWriter(client.GetStream());
+                writer.Write("post " + _n);
+                writer.Flush();
+                writer.Close();
+                client.Close();
+            }
+            catch (SocketException)
+            {
+                // Log the error
+            }
+            catch (IOException)
+            {
+                // Log the error
+            }
+        }
+    }
+
+}
